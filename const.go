f792f0d89a368bb6551c35b2cdd940950ded5ad4
Ainsley Clark <info@ainsleyclark.com>
2022-07-04 18:45:29 +0100

    Worker proto

---
+++ const.go
@@ -0,0 +1,14 @@
+// Copyright 2020 The Reddico Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package krangio
+
+const (
+	// LogDatabase defines the database name for log entries
+	// via Mongo.
+	LogDatabase = "logs"
+	// LogService defines the collection name for log entries
+	// via Mongo.
+	LogService = "api"
+)
