3a43b2c12c1e519bb9a0021ae774f4f66eb7be01
Ainsley Clark <info@ainsleyclark.com>
2022-08-22 08:53:13 +0100

    Updated types

--- types.go
+++ types.go
@@ -28,6 +28,7 @@ type (
 		Status         PageStatus          `json:"status" bson:"status"`
 		UpdatedAt      time.Time           `json:"updated_at" bson:"updated_at"`
 		CreatedAt      time.Time           `json:"created_at" bson:"created_at"`
+		LambdaError    any                 `json:"lambda_error" bson:"-"`
 	} //@name Page
 	// Scrape represents an individual scrape of a page and its
 	// various metrics.
